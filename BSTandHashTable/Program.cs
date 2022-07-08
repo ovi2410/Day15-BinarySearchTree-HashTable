3335b7cbad8b29e6d5aa6cc524ff0172297fb341
ovi2410 <guneshwarifegade@gmail.com>
2022-07-08 15:50:29 +0530

    [ovi]Add:UC2_CreateBST_FindSize

--- BSTandHashTable/Program.cs
+++ BSTandHashTable/Program.cs
@@ -3,22 +3,15 @@
 
 using BSTandHashTable;
 
-Console.WriteLine("----- Hash table -----\n");
+Console.WriteLine("----- Binary_Search_Tree -----\n");
 
-MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-//hash.Add("0", "To");
-//hash.Add("1", "be");
-//hash.Add("2", "or");
-//hash.Add("3", "not");
-//hash.Add("4", "to");
-//hash.Add("5", "be");
-////string hash5 = hash.Get("5");
-////Console.WriteLine("5th index value: " + hash5);
-////string hash2 = hash.Get("2");
-////Console.WriteLine("2th index value: " + hash2);
+BinarySearchTreeOps<int> binarySearchTree = new BinarySearchTreeOps<int>(56); //Root=56  
 
-//hash.Display("5");
-//hash.Display("2");
+binarySearchTree.InsertMultiItems(30, 70, 22, 40);
 
-hash.GetFreq();
+Console.WriteLine("\n>> Binary_Search_Tree :- ");
+binarySearchTree.Display();
+
+binarySearchTree.GetSizeOfBSt(binarySearchTree);
+
 Console.ReadKey();
