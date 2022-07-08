9e248dabeacb957704e1f2e6f998c75163da2c35
ovi2410 <guneshwarifegade@gmail.com>
2022-07-08 15:46:19 +0530

    [ovi]Add:UC1_CreateHashTable_FreqOfItem

--- BSTandHashTable/Program.cs
+++ BSTandHashTable/Program.cs
@@ -3,13 +3,22 @@
 
 using BSTandHashTable;
 
-Console.WriteLine("----- Binary_Search_Tree -----\n");
+Console.WriteLine("----- Hash table -----\n");
 
-BinarySearchTreeOps<int> binarySearchTree = new BinarySearchTreeOps<int>(56); //Root=56  
-binarySearchTree.Insert(30);
-binarySearchTree.Insert(70);
+MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+//hash.Add("0", "To");
+//hash.Add("1", "be");
+//hash.Add("2", "or");
+//hash.Add("3", "not");
+//hash.Add("4", "to");
+//hash.Add("5", "be");
+////string hash5 = hash.Get("5");
+////Console.WriteLine("5th index value: " + hash5);
+////string hash2 = hash.Get("2");
+////Console.WriteLine("2th index value: " + hash2);
 
-Console.WriteLine("\n>> Binary_Search_Tree :- ");
-binarySearchTree.Display();
+//hash.Display("5");
+//hash.Display("2");
 
+hash.GetFreq();
 Console.ReadKey();
